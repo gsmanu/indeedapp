89c7b1a24bcbb658f3e01a83215441e653f5ade0
gsmanu <gsmanu007@gmail.com>
2014-08-12 01:06:31 +0530

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ indeedapp
 =========
 
 App made of Indeed API, using Angular JS
+
+### Running it locally
+Make sure you have python installed in your system.
+Go into the `indeed` folder via the terminal using `cd indeed` command (UNIX or Mac).
+Type `python -m SimpleHTTPServer`
+Now open http://localhost:8000 to visit the app.
---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+var indeedApp = angular.module('indeedApp', []);
+
+indeedApp.controller('JobListCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('/data/jobs.json').
+    success(function(data, status, headers, config) {
+	$scope.status = status;
+        $scope.data = data;
+	$scope.jobs = $scope.data.results;
+}).
+    error(function(data, status, headers, config) {
+	$scope.data = data || "Request failed";
+        $scope.status = status;
+});
+}]);
